4758bb6e6af11eef70fba6f3071b36b6434d37d4
isa-id <juanse.giraldo07@gmail.com>
2024-11-17 13:11:49 -0500

    Subida de la rama de contactos - JSGC

--- TiendaPegassi/src/main.jsx
+++ TiendaPegassi/src/main.jsx
@@ -1,11 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { AppContact } from './Contact/App'
+import { AppContact } from './Contact/AppContact'
 import { IndexP } from './LandingPage/IndexP'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <IndexP/>
+    {/* <IndexP/> */}
+    <AppContact/>
   </StrictMode>,
 )
