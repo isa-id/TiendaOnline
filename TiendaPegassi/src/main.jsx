ede39c1cde5ed81fe7712d2ddede2d6dd241927c
isa-id <juanse.giraldo07@gmail.com>
2024-11-21 14:41:58 -0500

    Organizando LP

--- TiendaPegassi/src/main.jsx
+++ TiendaPegassi/src/main.jsx
@@ -1,13 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { AppContact } from './Contact/AppContact'
+import { AppContact } from './Contact/App'
 import { IndexP } from './LandingPage/IndexP'
-
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <IndexP/> */}
-    <AppContact/>
+    <IndexP/>
   </StrictMode>,
 )
